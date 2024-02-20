62545749ecec6b40ede7a7cef1d9d920f045c020
Densamisten <156061795+Densamisten@users.noreply.github.com>
2024-02-20 18:05:54 +0000

    Initial commit

---
+++ src/main/java/io/github/densamisten/FabrientDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.densamisten;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FabrientDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
